b441c43c8f2e0d92b21c59f8d3923412c536a835
everlastw <87893054+everlastw@users.noreply.github.com>
2022-07-22 20:22:21 +0400

    Add raycaster

--- src/components/ThreeDView.tsx
+++ src/components/ThreeDView.tsx
@@ -5,6 +5,8 @@ import {
     WebGLRenderer,
     //sRGBEncoding,
     Vector3,
+    Vector2,
+    Raycaster,
 } from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -22,17 +24,27 @@ export default class ThreeDView extends React.Component<ThreeDViewProps>{
     camera: any = undefined;
     renderer: any = undefined;
     controls: any = undefined;
+    raycaster: any = undefined;
+    pointer: any = undefined;
+    onPointerMove: any = undefined;
 
     constructor(props:ThreeDViewProps){
         super(props);
         var {width, height} = props;
         this.camera = new PerspectiveCamera( 75, width/height, 0.1, 1000 );
         this.camera.position.y = 30;
-        this.camera.lookAt(new Vector3(0,0,0))
+        this.camera.lookAt(new Vector3(0,0,0));
         this.renderer = new WebGLRenderer();
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-        // this.renderer.outputEncoding = sRGBEncoding;
-        // this.renderer.setPixelRatio(  );
+        this.raycaster = new Raycaster();
+        this.pointer = new Vector2();
+        const pointer = this.pointer;
+        this.onPointerMove = function ( event: any ) {
+            const x = event.pageX - event.currentTarget.offsetLeft; 
+            const y = event.pageY - event.currentTarget.offsetTop; 
+            pointer.x = x / width * 2 - 1
+            pointer.y = - y / height * 2 + 1
+        }
         this.view = React.createRef();
         this.renderer.setSize( width, height );
     }
@@ -48,6 +60,7 @@ export default class ThreeDView extends React.Component<ThreeDViewProps>{
             return 
         }
         const elem = this.view.current;
+        elem.addEventListener( 'pointermove', this.onPointerMove );
         while(elem.firstChild) {
             elem.removeChild(elem.lastChild);
         }
@@ -61,8 +74,15 @@ export default class ThreeDView extends React.Component<ThreeDViewProps>{
         const renderer = this.renderer;
         const camera = this.camera;
         const controls = this.controls;
+        const raycaster = this.raycaster;
+        const pointer = this.pointer;
         function animate() {
             requestAnimationFrame( animate );
+            raycaster.setFromCamera( pointer, camera );
+            const intersects = raycaster.intersectObjects( scene.children );
+            for ( let i = 0; i < intersects.length; i ++ ) {
+                intersects[ i ].object.material.color.set( 0xff0000 );
+            }
             controls.update();
             renderer.render( scene, camera );
         }
